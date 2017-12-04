5f9781f11fe4ee0f907b5ea5d99e00980b1c7202
Ray Dai <raydai@psmail.net>
2017-12-04 09:23:57 +0800

    updated the feed URL to new server

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,7 +7,6 @@ import { AudioProvider } from 'ionic-audio';
 import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
 import { File } from '@ionic-native/file';
 import { SocialSharing} from '@ionic-native/social-sharing';
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -25,84 +24,85 @@ export class HomePage {
 
   constructor(public navCtrl: NavController, public http: Http, private _audioProvider: AudioProvider, private transfer: FileTransfer, private file: File, public socialSharing: SocialSharing) {
     //this.http.get('http://kmclearvoice.com/rss2json/feed2.json').map(res => res.json()).subscribe(data => {
-    this.http.get('http://www.kmclearvoice.com/lifepod/json/lifepod1.json').map(res => res.json()).subscribe(data => {
+    // this.http.get('http://www.kmclearvoice.com/lifepod/json/lifepod1.json').map(res => res.json()).subscribe(data => {
+    this.http.get('https://www.surehope.net/lifepod/json/hmongradio.json').map(res => res.json()).subscribe(data => {
 
       for (let ind_item of data.item) { //loop through feed items, push to the new array
-       console.log("first..." + ind_item.link )
-       
-       //HomePage.fileName = ind_item.link; //default to remote
-       //HomePage.localName = ind_item.link.replace(/^http:\/\/(.*).mp3/g,"$1").replace(/[\.\/]/g, "_") + ".mp3";
+        console.log("first..." + ind_item.link)
 
-       //console.log(this.file.dataDirectory+ HomePage.localName)
- //this.file.checkFile(this.file.dataDirectory, HomePage.localName).then(_ => console.log('File exists')).catch(err => console.log('Directory doesnt exist'));
+        //HomePage.fileName = ind_item.link; //default to remote
+        //HomePage.localName = ind_item.link.replace(/^http:\/\/(.*).mp3/g,"$1").replace(/[\.\/]/g, "_") + ".mp3";
 
-    var url = this.getSourceName(ind_item.link, ind_item.description, ind_item.title, ind_item.date);
-     //console.log(url);
-      
-/*      this.file.checkFile(this.file.externalDataDirectory, HomePage.localName).then(_ => { //if file exists locally, used the downloaded copy
-        //HomePage.fileName = (this.file.dataDirectory + this.convertURLtoFileName(ind_item.link)),
-    
-        //  HomePage.fileName = (this.file.dataDirectory + this.convertURLtoFileName(ind_item.link))
-        console.log("found local file..." +  this.file.externalDataDirectory+HomePage.localName)  
-        //HomePage.fileName = this.file.externalDataDirectory + HomePage.localName;
-              console.log("final path..." + HomePage.fileName);
-       }).catch(_ => {
-         console.log("notfound");
-               console.log("final path..." + HomePage.fileName);
-         })*/
-                
+        //console.log(this.file.dataDirectory+ HomePage.localName)
+        //this.file.checkFile(this.file.dataDirectory, HomePage.localName).then(_ => console.log('File exists')).catch(err => console.log('Directory doesnt exist'));
+
+        var url = this.getSourceName(ind_item.link, ind_item.description, ind_item.title, ind_item.date);
+        //console.log(url);
+
+        /*      this.file.checkFile(this.file.externalDataDirectory, HomePage.localName).then(_ => { //if file exists locally, used the downloaded copy
+                //HomePage.fileName = (this.file.dataDirectory + this.convertURLtoFileName(ind_item.link)),
+            
+                //  HomePage.fileName = (this.file.dataDirectory + this.convertURLtoFileName(ind_item.link))
+                console.log("found local file..." +  this.file.externalDataDirectory+HomePage.localName)  
+                //HomePage.fileName = this.file.externalDataDirectory + HomePage.localName;
+                      console.log("final path..." + HomePage.fileName);
+               }).catch(_ => {
+                 console.log("notfound");
+                       console.log("final path..." + HomePage.fileName);
+                 })*/
+
 
 
         //if file exists, src is local file, otherwise leave URL for streaming
-   /*    this.myTracks.push({
-          src: HomePage.fileName,
-          artist: ind_item.description,
-          title: ind_item.title,
-          art: ind_item.description,
-          airDate: ind_item.date,
-          preload: 'metadata' // tell the plugin to preload metadata such as duration for this track, set to 'none' to turn off
-        })*/
+        /*    this.myTracks.push({
+               src: HomePage.fileName,
+               artist: ind_item.description,
+               title: ind_item.title,
+               art: ind_item.description,
+               airDate: ind_item.date,
+               preload: 'metadata' // tell the plugin to preload metadata such as duration for this track, set to 'none' to turn off
+             })*/
 
-/*       });*/
-    //  });
+        /*       });*/
+        //  });
       }
     });
   }
 
-  getSourceName(url, desc, title, date){
+  getSourceName(url, desc, title, date) {
     //comment
     console.log("arrived")
-     this.file.resolveLocalFilesystemUrl(this.file.dataDirectory+url.replace(/^http:\/\/(.*).mp3/g,"$1").replace(/[\.\/]/g, "_") + ".mp3").then(res => { //if file exists locally, used the downloaded copy
-        //HomePage.fileName = (this.file.dataDirectory + this.convertURLtoFileName(ind_item.link)),
-    
-        //  HomePage.fileName = (this.file.dataDirectory + this.convertURLtoFileName(ind_item.link))
-        console.log("found local file..." +  res.toURL())  
-        //HomePage.fileName = this.file.externalDataDirectory + HomePage.localName;
-         //     console.log("final path..." + HomePage.fileName);
-         console.log(res.toURL())
-        // return res.toURL()
-        this.myTracks.push({
-          src: res.toURL(),
-          artist: desc,
-          title: title,
-          art: desc,
-          airDate: date,
-          preload: 'metadata' // tell the plugin to preload metadata such as duration for this track, set to 'none' to turn off
-        })
-       }, err => {
-         console.log("remote..." + url)
-       //return url.toURL();
-       this.myTracks.push({
-          src: url,
-          artist: desc,
-          title: title,
-          art: desc,
-          airDate: date,
-          preload: 'metadata' // tell the plugin to preload metadata such as duration for this track, set to 'none' to turn off
-        })
+    this.file.resolveLocalFilesystemUrl(this.file.dataDirectory + url.replace(/^http:\/\/(.*).mp3/g, "$1").replace(/[\.\/]/g, "_") + ".mp3").then(res => { //if file exists locally, used the downloaded copy
+      //HomePage.fileName = (this.file.dataDirectory + this.convertURLtoFileName(ind_item.link)),
+
+      //  HomePage.fileName = (this.file.dataDirectory + this.convertURLtoFileName(ind_item.link))
+      console.log("found local file..." + res.toURL())
+      //HomePage.fileName = this.file.externalDataDirectory + HomePage.localName;
+      //     console.log("final path..." + HomePage.fileName);
+      console.log(res.toURL())
+      // return res.toURL()
+      this.myTracks.push({
+        src: res.toURL(),
+        artist: desc,
+        title: title,
+        art: desc,
+        airDate: date,
+        preload: 'metadata' // tell the plugin to preload metadata such as duration for this track, set to 'none' to turn off
       })
+    }, err => {
+      console.log("remote..." + url)
+      //return url.toURL();
+      this.myTracks.push({
+        src: url,
+        artist: desc,
+        title: title,
+        art: desc,
+        airDate: date,
+        preload: 'metadata' // tell the plugin to preload metadata such as duration for this track, set to 'none' to turn off
+      })
+    })
   }
-    
+
   ngAfterContentInit() {
     // get all tracks managed by AudioProvider so we can control playback via the API
     this.allTracks = this._audioProvider.tracks;
@@ -122,32 +122,33 @@ export class HomePage {
     console.log('Track finished', track)
   }
 
-  convertURLtoFileName(url){
-    return url.replace(/^http:\/\/(.*).mp3/g,"$1").replace(/[\.\/]/g, "_")+".mp3"; //converting the URL to a filename
+  convertURLtoFileName(url) {
+    return url.replace(/^http:\/\/(.*).mp3/g, "$1").replace(/[\.\/]/g, "_") + ".mp3"; //converting the URL to a filename
   }
-    private fileTransfer: FileTransferObject = this.transfer.create();
+  private fileTransfer: FileTransferObject = this.transfer.create();
 
-    public async start_download(url): Promise<void> {
-          //const url = 'http://jbharrison62966.podbean.com/mf/feed/quh5k7/The_Harvest.mp3';
-          //const url = 'https://www.kmclearvoice.com/joy.mp3';          
-          console.log('download starting... '+url);
-          console.log(this.file.dataDirectory + this.convertURLtoFileName(url));
-          this.fileTransfer.download(url, this.file.dataDirectory + this.convertURLtoFileName(url)).then((entry) => {
-            console.log('download complete: ' + entry.toURL());
-          }, (error) => {
-            // handle error
-            console.log('error ');
-          });
+  public async start_download(url): Promise < void > {
+    //const url = 'http://jbharrison62966.podbean.com/mf/feed/quh5k7/The_Harvest.mp3';
+    //const url = 'https://www.kmclearvoice.com/joy.mp3';          
+    console.log('download starting... ' + url);
+    console.log(this.file.dataDirectory + this.convertURLtoFileName(url));
+    this.fileTransfer.download(url, this.file.dataDirectory + this.convertURLtoFileName(url)).then((entry) => {
+      console.log('download complete: ' + entry.toURL());
+    }, (error) => {
+      // handle error
+      console.log('error ');
+    });
 
   }
 
   shareApp(): void {
-    
-       this.socialSharing.share('Download Here','App', null,'http://www.kmclearvoice.com/lifepod/apk/lifepod.apk').then(() => {
-          // Successfile:///data/app/com.yourapppackagename/base.apk
-          console.log('success!');
-        }).catch(() => {
-            // Error!
-          });
-        }
+
+    this.socialSharing.share('Download Here', 'App', null, 'http://www.kmclearvoice.com/lifepod/apk/lifepod.apk').then(() => {
+      // Successfile:///data/app/com.yourapppackagename/base.apk
+      console.log('success!');
+    }).catch(() => {
+      // Error!
+    });
+  }
 }
+
